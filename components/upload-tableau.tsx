04c7707839de4533a07a2287fb6e7b3ac88e332b
dsavastyan <160033999+dsavastyan@users.noreply.github.com>
2024-12-08 19:33:16 +0300

    Update upload-tableau.tsx

--- components/upload-tableau.tsx
+++ components/upload-tableau.tsx
@@ -1,37 +1,45 @@
-'use client'
+'use client';
 
 import React, { useEffect } from 'react';
 
 const TableauEmbed: React.FC = () => {
   useEffect(() => {
-    // Add the Tableau visualization script when the component mounts
-    const scriptElement = document.createElement('script');
-    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
-    document.body.appendChild(scriptElement);
+    // Dynamically load the Tableau JS API script
+    const script = document.createElement('script');
+    script.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+    script.async = true;
+    document.body.appendChild(script);
 
-    // Adjust the size of the Tableau visualization when the component mounts
-    const divElement = document.getElementById('viz1733674899534');
-    const vizElement = divElement?.getElementsByTagName('object')[0];
-    
-    if (divElement && vizElement) {
-      if (divElement.offsetWidth > 800) {
-        vizElement.style.width = '1366px';
-        vizElement.style.height = '795px';
-      } else if (divElement.offsetWidth > 500) {
-        vizElement.style.width = '1366px';
-        vizElement.style.height = '795px';
-      } else {
-        vizElement.style.width = '100%';
-        vizElement.style.height = '1877px';
+    script.onload = () => {
+      // Initialize Tableau Viz once the script has loaded
+      const vizDiv = document.getElementById('tableauViz') as HTMLDivElement;
+      if (vizDiv) {
+        const options = {
+          width: '100%',
+          height: '100%',
+          hideTabs: true,
+          hideToolbar: false,
+        };
+        const vizUrl = 'https://public.tableau.com/views/CourseProjectCourtStatistics2023/Dashboard1';
+        
+        new window.tableau.Viz(vizDiv, vizUrl, options);
       }
-    }
+    };
+
+    // Cleanup the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
     <div>
       <h1>Tableau Dashboard Example</h1>
-      {/* Tableau Embed Code */}
-      <div className="tableauPlaceholder" id="viz1733674899534" style={{ position: 'relative' }}>
+      {/* Tableau Embed Container */}
+      <div
+        id="tableauViz"
+        style={{ width: '100%', height: '600px', position: 'relative' }}
+      >
         <noscript>
           <a href="#">
             <img
@@ -41,21 +49,6 @@ const TableauEmbed: React.FC = () => {
             />
           </a>
         </noscript>
-        <object className="tableauViz" style={{ display: 'none' }}>
-          <param name="host_url" value="https%3A%2F%2Fpublic.tableau.com%2F" />
-          <param name="embed_code_version" value="3" />
-          <param name="site_root" value="" />
-          <param name="name" value="CourseProjectCourtStatistics2023/Dashboard1" />
-          <param name="tabs" value="no" />
-          <param name="toolbar" value="yes" />
-          <param name="static_image" value="https://public.tableau.com/static/images/Co/CourseProjectCourtStatistics2023/Dashboard1/1.png" />
-          <param name="animate_transition" value="yes" />
-          <param name="display_static_image" value="yes" />
-          <param name="display_spinner" value="yes" />
-          <param name="display_overlay" value="yes" />
-          <param name="display_count" value="yes" />
-          <param name="language" value="en-GB" />
-        </object>
       </div>
     </div>
   );
