b726aa41e1aef576a43401adbdef2b8c44bae093
dsavastyan <160033999+dsavastyan@users.noreply.github.com>
2024-12-14 16:59:21 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Link from "next/link"; 
 import { SiteHeader } from "@/components/site-header"
 import { Card, CardContent } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
